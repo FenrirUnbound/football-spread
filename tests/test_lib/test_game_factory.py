7ad7b0ba7f120b96f7f1b950e43f3208a85fdf4b
Darren M <aeneascorrupt@gmail.com>
2014-09-28 23:07:55 -0700

    remove parent key

--- tests/test_lib/test_game_factory.py
+++ tests/test_lib/test_game_factory.py
@@ -8,14 +8,17 @@ from google.appengine.ext import ndb
 
 class TestGameFactory(object):
     def generate_data(self, year=1999, week=0):
-        # parent_key = ndb.Key('year', year, 'week', week)
+        parent_key = ndb.Key('year', year, 'week', week)
         generated_game_id = self.__random_game_id()
 
-        return ScoreModel(
-            parent=ndb.Key('year', year, 'week', week),
-            week=week,
-            year=year,
-            game_id=generated_game_id)
+        result = ScoreModel(
+            week=int(week),
+            year=int(year),
+            game_id=int(generated_game_id)
+            )
+
+        print result.parent()
+        return result
 
     def __random_game_id(self):
         return random.randint(1000, 3000)
