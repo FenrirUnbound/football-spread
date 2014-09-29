9ff68a67aca17c48c579ab3274003d08d7a39feb
Darren M <aeneascorrupt@gmail.com>
2014-09-28 22:17:50 -0700

    generate game_id prior to object creation

--- tests/test_lib/test_game_factory.py
+++ tests/test_lib/test_game_factory.py
@@ -9,12 +9,13 @@ from google.appengine.ext import ndb
 class TestGameFactory(object):
     def generate_data(self, year=1999, week=0):
         parent_key = ndb.Key('year', year, 'week', week)
+        generated_game_id = self.__random_game_id()
 
         return ScoreModel(
             parent=parent_key,
             week=week,
             year=year,
-            game_id=self.__random_game_id()
+            game_id=generated_game_id
             )
 
     def __random_game_id(self):
